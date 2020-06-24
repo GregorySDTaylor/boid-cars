a732b1bd03423125d19813f070f384ad91ad9fe0
Greg Taylor <GregorySDTaylor@gmail.com>
2020-06-23 20:34:20 -0400

    added separation force

--- core/src/com/taylorbros/Bird.kt
+++ core/src/com/taylorbros/Bird.kt
@@ -3,12 +3,15 @@ package com.taylorbros
 import com.badlogic.gdx.Gdx
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer
 import com.badlogic.gdx.math.Vector2
+import kotlin.math.pow
 
 class Bird(
         override val position: Vector2,
         override val velocity: Vector2,
         override val localDistance: Float,
-        override val flockingPower: Float
+        override val flockingPower: Float,
+        private val maxSpeed: Float,
+        private val maxAcceleration: Float
 ) : Boid {
 
     val acceleration = Vector2()
@@ -18,6 +21,9 @@ class Bird(
         acceleration.add(separationForce(otherLocalBoids, localDistance))
         acceleration.add(alignmentForce(otherLocalBoids, localDistance))
         acceleration.add(cohesionForce(otherLocalBoids, localDistance))
+        if (acceleration.len() > maxAcceleration) { acceleration.setLength(maxAcceleration)}
+        velocity.add(acceleration)
+        if (velocity.len() > maxSpeed) { velocity.setLength(maxSpeed)}
         position.add(velocity)
         if (position.x > Gdx.graphics.width) {position.x = 0f}
         if (position.y > Gdx.graphics.height) {position.y = 0f}
@@ -26,23 +32,38 @@ class Bird(
     }
 
     private fun separationForce(otherLocalBoids: Set<Boid>, localDistance: Float): Vector2 {
-        return Vector2()
+        return if (otherLocalBoids.isEmpty()) {
+            Vector2()
+        }
+        else {
+            val separationForce = Vector2()
+            otherLocalBoids.forEach { other ->
+                val vectorAwayFromOther = this.position.cpy().sub(other.position)
+                val distance = vectorAwayFromOther.len()
+                val proportionOfLocalDistance = distance / localDistance
+                val inverseProportionOfLocalDistance = 1 - proportionOfLocalDistance
+                vectorAwayFromOther.setLength(inverseProportionOfLocalDistance)
+                separationForce.add(vectorAwayFromOther)
+            }
+            separationForce.scl(flockingPower / otherLocalBoids.count())
+        }
     }
 
     private fun alignmentForce(otherLocalBoids: Set<Boid>, localDistance: Float): Vector2 {
-        return Vector2()
+        val alignmentForce = Vector2()
+        return alignmentForce
     }
 
     private fun cohesionForce(otherLocalBoids: Set<Boid>, localDistance: Float): Vector2 {
-        return Vector2()
+        val cohesionForce = Vector2()
+        return cohesionForce
     }
 
     override fun render(shapeRenderer: ShapeRenderer) {
-        shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
+        shapeRenderer.set(ShapeRenderer.ShapeType.Filled);
         shapeRenderer.setColor(0.5f, 0.5f, 0.5f, 1f);
         shapeRenderer.circle(position.x, position.y, 5f);
         shapeRenderer.setColor(0.7f, 0.7f, 0.7f, 1f);
         shapeRenderer.line(position, position.cpy().add(velocity.cpy().scl(5f)))
-        shapeRenderer.end();
     }
 }
--- core/src/com/taylorbros/BoidCars.kt
+++ core/src/com/taylorbros/BoidCars.kt
@@ -9,10 +9,11 @@ import com.badlogic.gdx.math.Vector2
 
 class BoidCars : ApplicationAdapter() {
 
-    val boidCount = 200
-    val maxSpeed = 10f
-    val localDistance = 50f
-    val flockingPower = 1f
+    val boidCount = 300
+    val maxSpeed = 5f
+    val maxAcceleration = 1f
+    val localDistance = 100f
+    val flockingPower = 5f
 
     var shapeRenderer: ShapeRenderer? = null
     val boids = mutableSetOf<Boid>()
@@ -20,23 +21,26 @@ class BoidCars : ApplicationAdapter() {
 
     override fun create() {
         shapeRenderer = ShapeRenderer()
+        shapeRenderer!!.setAutoShapeType(true)
         repeat(boidCount) {
             var position = Vector2(
                     MathUtils.random() * Gdx.graphics.width,
                     MathUtils.random() * Gdx.graphics.height
             )
             var velocity = Vector2().setToRandomDirection().setLength(MathUtils.random() * maxSpeed)
-            boids.add(Bird(position, velocity, localDistance, flockingPower))
+            boids.add(Bird(position, velocity, localDistance, flockingPower, maxSpeed, maxAcceleration))
         }
     }
 
     override fun render() {
         boids.forEach {
             val otherLocalBoids = findOtherLocalBoids(it)
-            it.update(boids) }
+            it.update(otherLocalBoids) }
         Gdx.gl.glClearColor(0f, 0f, 0f, 1f)
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT)
+        shapeRenderer!!.begin();
         boids.forEach { it.render(shapeRenderer!!) }
+        shapeRenderer!!.end();
     }
 
     fun findOtherLocalBoids(principal : Boid) : Set<Boid> {
